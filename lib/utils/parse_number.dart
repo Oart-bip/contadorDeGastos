5dbe6464022befe1fb4afa25f273a8355ad6f67f
Arthur <arthurnantestrento@gmail.com>
2026-08-27 13:01:06 -0300

    parseNumber

--- lib/utils/parse_number.dart
+++ lib/utils/parse_number.dart
@@ -3,10 +3,13 @@ double? parseNumber(String text) {
 
   /* trim() remove espaços replaceAll troca vírgula por ponto
    o RegExp procura um numero inteiro ou decimal, como 10 ou 10.50 */
-  final regExp = RegExp(r'[0-9]+\.?[0-9]*');
+  final regExp = RegExp(r'[0-9]+\.?[0-9]*'); // * significa zero ou mais números depois do ponto
+  // com RegExp, definimos um padrão que a string digitada deverá ter ao ser verificada 
   final matches = regExp.allMatches(normalizedText).toList();
   if (matches.length != 1 || matches.first.group(0) != normalizedText) { // so aceita se existir um numero e ele corresponder ao texto inteiro
     return null;
   }
   return double.parse(matches.first.group(0)!); // converte o texto numerico encontrado para double
+
+  
 }
